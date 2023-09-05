9dc87583fc437240c0c8ff3fcc329685fba646ae
MCausc78 <mcausc78@gmail.com>
2023-09-06 02:36:09 +0300

    fix**

--- gorcon.go
+++ gorcon.go
@@ -47,8 +47,8 @@ func NewRCONClient(host string, port uint16) *RCONClient {
 func (client *RCONClient) SendPacket(packet Packet) error {
 	payload := []byte(packet.Data)
 	payloadLength := len(payload)
-	data := make([]byte, 16+payloadLength)
-	MCEndian.PutUint32(data, uint32(10+payloadLength))
+	data := make([]byte, 15+payloadLength)
+	MCEndian.PutUint32(data, uint32(11+payloadLength))
 	MCEndian.PutUint32(data[4:], uint32(packet.RequestID))
 	MCEndian.PutUint32(data[8:], uint32(packet.Type))
 	copy(data[12:], payload)
