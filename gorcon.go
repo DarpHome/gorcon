e0bf10c8a05fbe14f1fc6f7a0bb036e267d978c8
MCausc78 <mcausc78@gmail.com>
2023-09-06 02:41:27 +0300

    fix***

--- gorcon.go
+++ gorcon.go
@@ -73,7 +73,14 @@ func (client *RCONClient) RecvPacket() (*Packet, error) {
 	packet := &Packet{}
 	packet.RequestID = int32(MCEndian.Uint32(rawPacket))
 	packet.Type = PacketType(MCEndian.Uint32(rawPacket[4:]))
-	packet.Data = string(rawPacket[8:(packetLength - 9)])
+	data := rawPacket[8:]
+	packet.Data = ""
+	for _, r := range data {
+		if r == 0 {
+			break
+		}
+		packet.Data += string(r)
+	}
 	return packet, nil
 }
 
